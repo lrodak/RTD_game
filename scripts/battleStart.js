6ef9c8d3188ddd1ae8210a622eef3bf8de58a503
lrodak <rodao.lukasz@gmail.com>
2021-08-29 00:11:25 +0200

    damage information panel
    - fixed damage information panel
    - added 4 slots for skills

--- scripts/battleStart.js
+++ scripts/battleStart.js
@@ -3,11 +3,16 @@ function battleStart(){
     var hp = hp_label.split('/');
     var damage = calculateDamage();
     hp[0]=hp[0]-damage;
-    document.getElementById("opponent_hp_label").innerHTML = hp[0]+"/"+hp[1]
+    document.getElementById("opponent_hp_label").innerHTML = hp[0]+"/"+hp[1];
 
     if( hp[0]<=0){
         battleWin();
     }
 
-    document.getElementsByClassName("information_dmg").style.display = block;
+    // document.getElementsByClassName("information_dmg").style.display = 'block';
+    document.getElementById("dmg_information").style.display = 'inline';
+    document.getElementById("final_dmg").style.display = 'inline';
+    document.getElementById("label_information_dmg").style.display = 'inline';
+    
+    document.getElementById("final_dmg").innerHTML = damage;
 }
--- scripts/calculateDamage.js
+++ scripts/calculateDamage.js
@@ -5,5 +5,5 @@ function calculateDamage(){
     var player_damage_diff=player_damage[2]-player_damage[0];
 
     var dmg= Math.floor(Math.random() * player_damage_diff) + parseInt(player_damage[0]);
-    return 100+dmg;
+    return dmg;
 }
